762f6124195388046404e4b26d892c19232c0b2f
Antoine Toulme <antoine@lunar-ocean.com>
2019-12-13 11:39:48 -0800

    showcase eip152 vector doesn't pass with sodium GenericHash

---
+++ crypto/src/test/java/org/apache/tuweni/crypto/sodium/Eip152Test.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.apache.tuweni.crypto.sodium;
+
+import org.apache.tuweni.bytes.Bytes;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class Eip152Test {
+
+    @BeforeAll
+    static void checkAvailable() {
+        assumeTrue(Sodium.isAvailable(), "Sodium native library is not available");
+    }
+
+    @Test
+    void hashValue() {
+        GenericHash.Hash output = GenericHash.hash(64, GenericHash.Input.fromBytes(Bytes.fromHexString("0000000048c9bdf267e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d182e6ad7f520e511f6c3e2b8c68059b6bbd41fbabd9831f79217e1319cde05b61626300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000001")));
+        assertNotNull(output);
+        assertEquals(64, output.bytes().size());
+        assertEquals(Bytes.fromHexString("08c9bcf367e6096a3ba7ca8485ae67bb2bf894fe72f36e3cf1361d5f3af54fa5d282e6ad7f520e511f6c3e2b8c68059b9442be0454267ce079217e1319cde05b"), output.bytes());
+    }
+}
